69e77e110a1d620bf72781aa8e3851a51ccd6a04
Jason Casares <jasonmcasares@gmail.com>
2024-08-28 13:11:00 -0600

    glue job submission

--- include/scripts/nasa_backfill.py
+++ include/scripts/nasa_backfill.py
@@ -1,33 +1,33 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_date, year, month, dayofmonth
 import requests
+import tempfile
+import os
 from io import StringIO
 
 # S3 path for Iceberg data
 s3_bucket = "fire-mitigation-data"
-s3_path = f"s3://{s3_bucket}/iceberg-data"
+s3_path = f"s3://{s3_bucket}/nasa_firms"
 
 spark = SparkSession.builder \
-   .appName("NASA FIRMS Data Processing") \
-    .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog") \
-    .config("spark.sql.catalog.spark_catalog.type", "hive") \
-    .config("spark.sql.catalog.aws", "org.apache.iceberg.spark.SparkCatalog") \
-    .config("spark.sql.catalog.aws.type", "hadoop") \
-    .config("spark.sql.catalog.aws.warehouse", s3_path) \
-    .config("spark.sql.catalog.aws.s3.endpoint", "s3.amazonaws.com") \
+    .appName("NASA FIRMS Data Processing") \
     .getOrCreate()
 
 
 
 nasa_firms_url = "https://firms.modaps.eosdis.nasa.gov/data/country/modis/2023/modis_2023_United_States.csv"
 
-# Download the CSV file
+# Download the data and save it to a temporary file
 response = requests.get(nasa_firms_url)
-csv_data = StringIO(response.text)
+with tempfile.NamedTemporaryFile(delete=False, suffix='.csv', mode='wb') as temp_file:
+    temp_file.write(response.content)
+    temp_file_path = temp_file.name
+
+print(f"Temporary file path: {temp_file_path}")
 
 # Read the CSV data into a Spark DataFrame
-df = spark.read.csv(csv_data, header=True, inferSchema=True)
+df = spark.read.csv(temp_file_path, header=True, inferSchema=True)
+
 
 # Convert the timestamp column to a date
 df = df.withColumn("acq_date", to_date("acq_date", "yyyy-MM-dd")) \
@@ -36,12 +36,18 @@ df = df.withColumn("acq_date", to_date("acq_date", "yyyy-MM-dd")) \
        .withColumn("day", dayofmonth("acq_date"))
 
 # Write the data to an Iceberg table
-table_name = "aws.db.nasa_firms_data"
+table_name = "iceberg.nasa_firms_data"
 (df.writeTo(table_name)
-   .partitionBy("year", "month", "day")
+   .partitionedBy("year", "month", "day")
    .tableProperty("write.format.default", "parquet")
    .tableProperty("write.parquet.compression-codec", "snappy")
    .createOrReplace())
+
+# Delete the temporary file
+os.unlink(temp_file_path)
+
+# Read back and show some data to verify
+spark.table(table_name).show(5)
 
 # Stop the Spark session
 spark.stop()
